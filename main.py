c0eab36da2b3d09b04d790ba9a27567d15cc156d
Seu Nome <seuemail@exemplo.com>
2025-09-21 11:14:49 -0300

    Colocando o rodape na pasta certa

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-eix[clei
 
-]# This is a sample Python script.
+
+# This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
